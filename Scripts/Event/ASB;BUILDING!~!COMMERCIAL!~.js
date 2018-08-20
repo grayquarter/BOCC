158f26c89ccab9a0b0565de6dc3ae381311499dd
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-20 15:05:14 -0400

    Combined blocks

--- Scripts/Event/ASB;BUILDING!~!COMMERCIAL!~.js
+++ Scripts/Event/ASB;BUILDING!~!COMMERCIAL!~.js
@@ -1,22 +1,23 @@
-
 if (!appMatch('Building/Construction/Commercial/Shell') || !appMatch('Building/Trade Permits/Commercial/Irrigation System')) {
+
 	var LicProfList = aa.env.getValue('LicProfList').toArray();
 	var x = 0;
-}
 
-if ((!appMatch('Building/Trade Permits/Commercial/Irrigation System') || !appMatch('Building/Construction/Commercial/Shell')) && (LicProfList.length > 0)) {
-	for (x in LicProfList) {
-		var lpn = LicProfList[x].getLicenseNbr();
+	if (LicProfList.length > 0) {
+		for (x in LicProfList) {
+			var lpn = LicProfList[x].getLicenseNbr();
+		}
+		comment(GetLicInfo(lpn));
+		if (GetLicInfo(lpn)[0] == true) {
+			showMessage = true;
+			comment(GetLicInfo(lpn)[1]);
+			cancel = true;
+		}
 	}
-	comment(GetLicInfo(lpn)[1]);
-	if (GetLicInfo(lpn)[0] == true) {
-		// TODO: cancel by itself will not send a message to the user
+
+	if (LPValidation()) {
+		showMessage = true;
 		cancel = true;
+		comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
 	}
-}
-
-if ((!appMatch('Building/Trade Permits/Commercial/Irrigation System') || !appMatch('Building/Construction/Commercial/Shell')) && LPValidation()) {
-	showMessage = true;
-	cancel = true;
-	comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
 }
