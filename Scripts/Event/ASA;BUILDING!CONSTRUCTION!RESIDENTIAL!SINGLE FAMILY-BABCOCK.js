6f523102e83b053e97ad2e20358ee4c2d936ec73
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-20 15:23:42 -0400

    remove TEST comment

--- Scripts/Event/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
+++ Scripts/Event/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
@@ -2,5 +2,3 @@ if (!capId.isCreatedByACA()) {
 	createPendingInspection('RESSINGLE - B', 'Babcock Final');
 	scheduleInspection('Babcock Final', 100, 'AMY.WICKS@KIMLEY-HORN.COM', null, 'scheduled via script');
 }
-
-// test
