872e33980e9ac012e3d6e2e4596d102f765b71e7
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-20 14:59:35 -0400

    Combined blocks

--- Scripts/Event/ASB;BUILDING!~!RESIDENTIAL!~.js
+++ Scripts/Event/ASB;BUILDING!~!RESIDENTIAL!~.js
@@ -1,23 +1,25 @@
-
 if (!appMatch('Building/Trade Permits/Residential/Irrigation System') && !appMatch('Building/Trade Permits/Residential/Electrical')) {
+
 	var LicProfList = aa.env.getValue('LicProfList').toArray();
 	var x = 0;
-}
 
-if (!appMatch('Building/Trade Permits/Residential/Irrigation System') && !appMatch('Building/Trade Permits/Residential/Electrical') && (LicProfList.length > 0)) {
-	for (x in LicProfList) {
-		var lpn = LicProfList[x].getLicenseNbr();
+	if (LicProfList.length > 0) {
+
+		for (x in LicProfList) {
+			var lpn = LicProfList[x].getLicenseNbr();
+		}
+		comment(GetLicInfo(lpn));
+		if (GetLicInfo(lpn)[0] == true) {
+			showMessage = true;
+			comment(GetLicInfo(lpn)[1]);
+			cancel = true;
+		}
+
 	}
-	comment(GetLicInfo(lpn)[1]);
-	if (GetLicInfo(lpn)[0] == true) {
+
+	if (LPValidation()) {
 		showMessage = true;
-		comment(GetLicInfo(lpn)[1]);
 		cancel = true;
+		comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
 	}
-}
-
-if (!appMatch('Building/Trade Permits/Residential/Irrigation System') && !appMatch('Building/Trade Permits/Residential/Electrical') && LPValidation()) {
-	showMessage = true;
-	cancel = true;
-	comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
 }
