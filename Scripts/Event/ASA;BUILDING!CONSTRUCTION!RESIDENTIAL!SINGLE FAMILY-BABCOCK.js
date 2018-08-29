b911ba00da02c23890461a62a24b9d129136afa0
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-28 20:17:12 -0400

    added call to get cap output

--- Scripts/Event/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
+++ Scripts/Event/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
@@ -1,4 +1,6 @@
-if (!capId.isCreatedByACA()) {
+var mCap = aa.cap.getCap(capId).getOutput();
+
+if (!mCap.isCreatedByACA()) {
 	createPendingInspection('RESSINGLE - B', 'Babcock Final');
 	scheduleInspection('Babcock Final', 100, 'AMY.WICKS@KIMLEY-HORN.COM', null, 'scheduled via script');
 }
