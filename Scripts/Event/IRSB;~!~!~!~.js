93d836243afa0a62676d9ab1d9253bf067842567
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-29 16:42:18 -0400

    remove unwanted comment

--- Scripts/Event/IRSB;~!~!~!~.js
+++ Scripts/Event/IRSB;~!~!~!~.js
@@ -1,5 +1,4 @@
 showMessage = true;
-comment('This permit has a current status of:  ' + capStatus);
 if (inspType != 'On-Line Resubmittal' && inspType != 'Plans Change Submitted' && (appMatch('Building/*/*/*') && capStatus != 'Permit Issued' && capStatus != 'Issued' && capStatus != 'Temp C of O Issued')) {
 	comment("Cannot result inspections when the permit does not currently have a status of 'Issued'");
 	cancel = true;
