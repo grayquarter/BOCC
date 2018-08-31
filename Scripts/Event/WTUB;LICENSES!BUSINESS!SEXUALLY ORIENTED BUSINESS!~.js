8e04dbaf22da5b769d81c40e38624b956baf74e0
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-08-31 15:09:22 -0400

    added showMessage

--- Scripts/Event/WTUB;LICENSES!BUSINESS!SEXUALLY ORIENTED BUSINESS!~.js
+++ Scripts/Event/WTUB;LICENSES!BUSINESS!SEXUALLY ORIENTED BUSINESS!~.js
@@ -1,4 +1,4 @@
-
+showMessage = true;
 if (wfTask == 'Sufficiency Review' && wfStatus == 'Complete' && balanceDue > 0) {
 	comment('Fee Balance must be paid before completing this task.');
 	cancel = true;
