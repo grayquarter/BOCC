bbc3826ed64c0b85fc03656646827c1dca4360ba
klbocc <42004819+klBOCC@users.noreply.github.com>
2018-09-05 17:29:14 -0400

    added exceptions to validation

--- INCLUDES_CUSTOM/GetLicInfo.js
+++ INCLUDES_CUSTOM/GetLicInfo.js
@@ -4,11 +4,18 @@ function GetLicInfo(myLicNo) {
 		for (x in lpArr) {
 			var thisLp = lpArr[x];
 			var licStatusC = thisLp.getAuditStatus();
-			if (licStatusC == "A") {
-				var myInsInfo = GetLicInsInfo(thisLp, myLicNo);
-				return myInsInfo;
+			var licType = thisLp.getLicenseType();
+			if (licType == "OWNER BUILDER" || licType == "UNLICENSED CONTRACTOR" || licType == "COUNTY EMPLOYEE" || licType == "LP PENDING") {
+				logDebug("OWNER BUILDER, UNLICENSED CONTRACTOR, LP PENDING, COUNTY EMPLOYEE... bypassing LP check.");
+				var myInsInfo2 = [false, "Insurance not required for this license type."];
+				return myInsInfo2;
 			} else {
-				return "This is not a valid license.";
+				if (licStatusC == "A") {
+					var myInsInfo = GetLicInsInfo(thisLp, myLicNo);
+					return myInsInfo;
+				} else {
+					return "This is not a valid license.";
+				}
 			}
 		}
 	} else {
