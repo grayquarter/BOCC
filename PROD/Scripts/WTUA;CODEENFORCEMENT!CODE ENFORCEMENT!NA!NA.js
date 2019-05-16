ab3b71108b0e149bd6998ef48838f47637908146
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:09:21 -0400

    commit by emse tools 2019-05-16 12:09:21

--- PROD/Scripts/WTUA;CODEENFORCEMENT!CODE ENFORCEMENT!NA!NA.js
+++ PROD/Scripts/WTUA;CODEENFORCEMENT!CODE ENFORCEMENT!NA!NA.js
@@ -1,3 +1,4 @@
+//WTUA:CODEENFORCEMENT/CODE ENFORCEMENT/NA/NA
 
 if (wfTask == 'Notification' && wfStatus == 'Lien Assessed') {
 	addFee('CODEBRD', 'CODE_ENFORCEMENT', 'ORIGINAL', AInfo['Lien Assessed'], 'Y');
