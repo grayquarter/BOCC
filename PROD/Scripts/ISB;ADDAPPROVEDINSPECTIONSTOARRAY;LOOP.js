f488e10ee99c02e34eaf5634aa71be846107c19e
klbocc <42004819+klBOCC@users.noreply.github.com>
2019-05-16 13:38:59 -0400

    Delete as standard choice
    ISB;ADDAPPROVEDINSPECTIONSTOARRAY;LOOP.js

--- PROD/Scripts/ISB;ADDAPPROVEDINSPECTIONSTOARRAY;LOOP.js
+++
@@ -1,6 +0,0 @@
-
-capApprovedInspectionType = inspectionTypes[type].getInspectionType();
-capInspectionStatus = inspectionTypes[type].getInspectionStatus();
-if (matches(capInspectionStatus, 'Pass', 'Approved as Noted', 'Approved', 'Approved with Conditions', 'Not Required')) {
-	arrApprovedInspections.push(capApprovedInspectionType);
-}
