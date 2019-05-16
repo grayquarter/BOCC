2906b5ec5837b006778af00ef821f09ea932a4a3
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:10:05 -0400

    commit by emse tools 2019-05-16 12:10:05

--- PROD/Scripts/WTUB;CODEENFORCEMENT!COMPLAINTS!~!~.js
+++ PROD/Scripts/WTUB;CODEENFORCEMENT!COMPLAINTS!~!~.js
@@ -1,5 +1,7 @@
+//WTUB:CODEENFORCEMENT/COMPLAINTS//
 
 if (wfStatus == 'Not in Compliance' && wfTask == 'Code Violation' && AInfo['Updated.CILB'] != 'CHECKED' && AInfo['Updated.CE'] != 'CHECKED' && AInfo['Updated.Unsafe Building'] != 'CHECKED') {
-	comment('If Not in Compliance status selected, please check CE, CILB or Unsafe Building ');
-	cancel = true;
+    showMessage = true;
+    comment('If Not in Compliance status selected, please check CE, CILB or Unsafe Building ');
+    cancel = true;
 }
