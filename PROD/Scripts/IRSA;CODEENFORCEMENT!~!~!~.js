8d8ef7acad659baa62058714418b9a62ff1f11eb
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:01:48 -0400

    commit by emse tools 2019-05-16 12:01:48

--- PROD/Scripts/IRSA;CODEENFORCEMENT!~!~!~.js
+++ PROD/Scripts/IRSA;CODEENFORCEMENT!~!~!~.js
@@ -1,6 +1,12 @@
+//IRSA:CODEENFORCEMENT///  refactored
 //start replaced branch: CC_151_CE_InspResultAfter
-{
-	comment('CC_151_CE_InspResultAfter Executing message');
 
+comment('CC_151_CE_InspResultAfter Executing message');
+
+
+if (inspResult.indexOf('Fail') > -1 || inspResult.indexOf('Partial') > -1 || inspResult.indexOf('Cancelled') > -1) {
+    createPendingInspection(inspGroup, inspType);
 }
+
+
 //end replaced branch: CC_151_CE_InspResultAfter;
