300677ca6174880edc58bb16764daa6ae4db1ad3
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 10:31:40 -0400

    commit by emse tools 2019-05-16 10:31:40

--- PROD/Scripts/ASB;PLANNING!GROWTH MGMT!ZONING PERMITS!RESIDENTIAL FENCE.js
+++ PROD/Scripts/ASB;PLANNING!GROWTH MGMT!ZONING PERMITS!RESIDENTIAL FENCE.js
@@ -1,21 +1,22 @@
+//ASB:PLANNING/GROWTH MGMT/ZONING PERMITS/RESIDENTIAL FENCE
 
 var LicProfList = aa.env.getValue('LicProfList').toArray();
 var x = 0;
 if ((LicProfList.length > 0)) {
-	for (x in LicProfList) {
-		var lpn = LicProfList[x].getLicenseNbr();
-	}
-	comment(GetLicInfo(lpn)[1]);
-	if (GetLicInfo(lpn)[0] == true) {
-		showMessage = true;
-		comment(GetLicInfo(lpn)[1]);
-		cancel = true;
-
-	}
+    for (x in LicProfList) {
+        var lpn = LicProfList[x].getLicenseNbr();
+    }
+    comment(GetLicInfo(lpn)[1]);
+    if (GetLicInfo(lpn)[0] == true) {
+        //fixed TODO: cancel by itself will not send a message to the user
+            showMessage = true;
+            comment(GetLicInfo(lpn)[1]);
+            cancel = true;
+    }
 }
 
 if (LPValidation()) {
-	showMessage = true;
-	cancel = true;
-	comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
+    showMessage = true;
+    cancel = true;
+    comment('Your General liability or Workmans Compensation Insurance is expired or not on file. Please contact Community Development on 941-743-1201');
 }
