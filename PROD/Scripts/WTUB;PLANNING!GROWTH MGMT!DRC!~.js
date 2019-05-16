8462f6553dd4f738db3a7704a99f418682503111
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:10:22 -0400

    commit by emse tools 2019-05-16 12:10:22

--- PROD/Scripts/WTUB;PLANNING!GROWTH MGMT!DRC!~.js
+++ PROD/Scripts/WTUB;PLANNING!GROWTH MGMT!DRC!~.js
@@ -1,3 +1,4 @@
+//WTUB:PLANNING/GROWTH MGMT/DRC/
 
 if (wfTask == 'Finaled' && matches(wfStatus, 'Approved with Conditions', 'Approved') && !balanceDue == 0) {
 	comment('All Fees must be paid before this action can take place.');
