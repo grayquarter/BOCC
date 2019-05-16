f70cf3b7bf27569cc0e8f36798b9a705f0b12690
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:02:56 -0400

    commit by emse tools 2019-05-16 12:02:56

--- PROD/Scripts/ISA;~!~!~!~.js
+++ PROD/Scripts/ISA;~!~!~!~.js
@@ -1,37 +1,38 @@
 // ISA:  code extracted from modified "ISA New" scripts
+
 showMessage = true;
 var capIDString = capId.getCustomID();
-var inspObj = aa.inspection.getInspection(capId, inspId).getOutput();
+var inspObj = aa.inspection.getInspection(capId, inspId).getOutput(); 
 var inspGroup = inspObj.getInspection().getInspectionGroup();
 //update comments
 var inComm = inspObj.getInspectionComments();
 if (inComm == null) {
-    inComm = "";
+	inComm = "";
 }
 var mySearch = /Permit/i;
 var result = mySearch.test(inComm);
 // TODO:  code should be reviewed.
 if (result == true) {
-    result = false;
+	result = false;
 } else {
-    var inComm2 = inspObj.setInspectionComments(capIDString + " (Permit); " + inComm);
-    aa.inspection.editInspection(inspObj)
+	var inComm2 = inspObj.setInspectionComments(capIDString + " (Permit); " + inComm);
+	aa.inspection.editInspection(inspObj)
 }
-
+	
 comment(capIDString);
 oInspList = aa.inspection.getInspections(capId);
 inspArray = oInspList.getOutput();
 comment(inspArray);
 if (inspArray.length > 0) {
-    for (insp in inspArray) {
-        //replaced branch(fullInsps)
-        fullInsps();
-    }
+	for (insp in inspArray) {
+		//replaced branch(fullInsps)
+		fullInsps();
+	}
 }
 
 var BabFin = checkInspectionResult('Babcock Final', 'Scheduled');
 if (((BabFin == true) && (inspType == 'Babcock Final'))) {
-    assignInspection(inspId, 'AMY.WICKS@KIMLEY-HORN.COM', capId);
+	assignInspection(inspId, 'AMY.WICKS@KIMLEY-HORN.COM', capId);
 }
 
 var HealthInsp = checkInspectionResult('Health Approval', 'Scheduled');
@@ -43,7 +44,7 @@ var cap = aa.cap.getCap(capId).getOutput();
 var CapTypeResult = cap.getCapType();
 coastLightR = checkInspectionResult('COASTAL LIGHTING ROUGH', 'Scheduled');
 coastLightF = checkInspectionResult('COASTAL LIGHTING FINAL', 'Scheduled');
-if (coastLightR == true && inspType == 'COASTAL LIGHTING ROUGH') {
+if (coastLightR == true  && inspType == 'COASTAL LIGHTING ROUGH' ) {
     addrResult = aa.address.getAddressByCapId(capId);
     var addrArray = new Array();
     addrArray = addrResult.getOutput();
