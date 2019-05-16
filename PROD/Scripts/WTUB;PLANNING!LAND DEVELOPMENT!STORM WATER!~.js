6ff1fe84f797f82ce6ad484c670c5823c9626c62
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 12:11:58 -0400

    commit by emse tools 2019-05-16 12:11:58

--- PROD/Scripts/WTUB;PLANNING!LAND DEVELOPMENT!STORM WATER!~.js
+++ PROD/Scripts/WTUB;PLANNING!LAND DEVELOPMENT!STORM WATER!~.js
@@ -1,5 +1,7 @@
+//WTUB:PLANNING/LAND DEVELOPMENT/STORM WATER/
 
 if (wfTask == 'Review' && wfStatus == 'Approved' && !balanceDue == 0) {
-	comment('All Fees must be paid before this action can take place.');
-	cancel = true;
+    showMessage = true;
+    comment('All Fees must be paid before this action can take place.');
+    cancel = true;
 }
