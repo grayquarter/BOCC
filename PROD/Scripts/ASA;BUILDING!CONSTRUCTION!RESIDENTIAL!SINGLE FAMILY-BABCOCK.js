d528eae773d558c3c33a1804530ca4b6b3774cdc
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-16 10:09:57 -0400

    commit by emse tools 2019-05-16 10:09:57

--- PROD/Scripts/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
+++ PROD/Scripts/ASA;BUILDING!CONSTRUCTION!RESIDENTIAL!SINGLE FAMILY-BABCOCK.js
@@ -1,7 +1,9 @@
+//ASA:BUILDING/CONSTRUCTION/RESIDENTIAL/SINGLE FAMILY-BABCOCK
+
 var mCap = aa.cap.getCap(capId).getOutput();
 var myMatch = capIDString.indexOf('TMP') !== -1;
 
 if (!mCap.isCreatedByACA() && currentUserID.substr(0, 6) != 'PUBLIC' && (myMatch == false)) {
-	createPendingInspection('RESSINGLE - B', 'Babcock Final');
-	scheduleInspection('Babcock Final', 100, 'AMY.WICKS@KIMLEY-HORN.COM', null, 'scheduled via script');
+    createPendingInspection('RESSINGLE - B', 'Babcock Final');
+    scheduleInspection('Babcock Final', 100, 'AMY.WICKS@KIMLEY-HORN.COM', null, 'scheduled via script');
 }
