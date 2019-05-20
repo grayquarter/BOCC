e5dcb9d3c09f622f6b4822b91faf112cfb88b616
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-05-20 10:35:31 -0400

    commit by emse tools 2019-05-20 10:35:31

--- PROD/Scripts/ASA;PLANNING!GROWTH MGMT!ZONING DETERMINATION LETTER!ZONING DETERMINATION LETTER.js
+++ PROD/Scripts/ASA;PLANNING!GROWTH MGMT!ZONING DETERMINATION LETTER!ZONING DETERMINATION LETTER.js
@@ -6,8 +6,3 @@ if ((asi == 'Yes')) {
     scheduleInspection('Follow-up Inspection', 365, null, null, 'scheduled via script');
 }
 
-// TODO more optimal to use asi = AInfo["AG Exemption"]; prior: asi = getAppSpecific('AG Exemption', capId);
-// DISABLED: ASA:Planning/Growth Mgmt/Zoning Determination Letter/Zoning Determination Letter:10
-// if (proximity('AGIS_CHARCO', 'Babcock_Lots', 1)) {
-// 	addStdCondition('CC PERMIT','Babcock Ranch');
-// 	}
