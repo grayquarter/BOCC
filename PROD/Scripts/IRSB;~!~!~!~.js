1e7cfa4dd8af7b66739740c5936d761a77c4fcc0
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-06-11 11:20:34 -0400

    commit by emse tools 2019-06-11 11:20:34

--- PROD/Scripts/IRSB;~!~!~!~.js
+++ PROD/Scripts/IRSB;~!~!~!~.js
@@ -6,3 +6,11 @@ if (inspType != 'On-Line Resubmittal' && inspType != 'Plans Change Submitted' &&
         comment("Cannot result inspections when the permit does not currently have a status of 'Issued'");
 	cancel = true;
 }
+
+
+if ((currentUserID != 'JONESTC' && currentUserID != 'LAPHAMK') && inspType == 'Assess Garbage' && inspResult == "Pass") {
+    aa.print("This inspection type cannot be manually resulted.");
+    comment("This inspection type cannot be manually resulted.");
+    cancel = true;
+}
+
