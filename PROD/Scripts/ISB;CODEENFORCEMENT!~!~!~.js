8981353894be699a96c97592d91978a4c0ac4ed9
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-07-22 13:16:30 -0400

    commit by emse tools 2019-07-22 13:16:30

--- PROD/Scripts/ISB;CODEENFORCEMENT!~!~!~.js
+++ PROD/Scripts/ISB;CODEENFORCEMENT!~!~!~.js
@@ -1,7 +1,79 @@
 //ISB:CODEENFORCEMENT///
 
-if ((appMatch('CodeEnforcement/Unsafe Building/NA/NA') && inspType == 'Follow-up Inspection' && balanceDue > 0)) {
+
+if (currentUserID == "LAPHAMK") {
     showMessage = true;
-    comment('Inspection cannot be scheduled because there is a balance of $' + balanceDue + ' due on the record.');
-    cancel = true;
+    showDebug = true;
+} else {
+    showMessage = false;
+    showDebug = false;
+}
+
+var vScriptName = aa.env.getValue("ScriptCode");
+var vEventName = aa.env.getValue("EventName");
+aa.print(vScriptName + "/" + vEventName);
+
+if (vScriptName == "InspectionMultipleScheduleBefore") {
+    aa.print("Processing InspectionMultipleScheduleBefore...");
+    var PermitID1Array = aa.env.getValue("PermitID1Array");
+    var PermitID2Array = aa.env.getValue("PermitID2Array");
+    var PermitID3Array = aa.env.getValue("PermitID3Array");
+    var InspectionIdArray = aa.env.getValue("InspectionIDArray");
+    aa.print("InspectionIdArray obj=" + InspectionIdArray);
+
+
+    for (x in InspectionIdArray) {
+        InspectionId = InspectionIdArray[x];
+        capResult = aa.cap.getCapID(PermitID1Array[x], PermitID2Array[x], PermitID3Array[x]);
+        if (capResult.getSuccess()) {
+            var capId = capResult.getOutput();
+            aa.print("capId: " + capId);
+            var capIDString = capId.getCustomID();
+            aa.print("capIDString : " + capIDString);
+
+            aa.print("InspectionIdArray = " + InspectionIdArray);
+            var inspObj = aa.inspection.getInspection(capId, InspectionId).getOutput();
+            aa.print("inspObj:" + inspObj);
+            var inspGroup = inspObj.getInspection().getInspectionGroup();
+            var inspType = inspObj.getInspection().getInspectionType();
+            aa.print(" inspGroup(arr)== " + inspGroup);
+            aa.print(" inspType(arr)== " + inspType);
+
+            doISBprocess(capId, capIDString, inspType);
+
+        }
+    }
+}
+
+
+if (vScriptName == "InspectionScheduleBefore") {
+    var PermitId1 = aa.env.getValue("PermitId1");
+    var PermitId2 = aa.env.getValue("PermitId2");
+    var PermitId3 = aa.env.getValue("PermitId3");
+    var inspType = aa.env.getValue("InspectionType");
+    aa.print(PermitId1 + PermitId2 + PermitId3);
+    aa.print("inspType=" + inspType);
+
+    capResult = aa.cap.getCapID(PermitId1, PermitId2, PermitId3);
+    if (capResult.getSuccess()) {
+        var capId = capResult.getOutput();
+        aa.print("capId: " + capId);
+        var capIDString = capId.getCustomID();
+        aa.print("capIDString : " + capIDString);
+
+        doISBprocess(capId, capIDString, inspType);
+
+    }
+
+}
+
+
+
+function doISBprocess(capId, capIDString, inspType) {
+
+    if ((appMatch('CodeEnforcement/Unsafe Building/NA/NA') && inspType == 'Follow-up Inspection' && balanceDue > 0)) {
+        showMessage = true;
+        comment('Inspection cannot be scheduled because there is a balance of $' + balanceDue + ' due on the record.');
+        cancel = true;
+    }
 }
