573e489d1510e963dabe5ea88cd0d37700aa360c
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-07-26 06:13:11 -0400

    commit by emse tools 2019-07-26 06:13:11

--- PROD/Scripts/CTRCA;~!~!~!~.js
+++ PROD/Scripts/CTRCA;~!~!~!~.js
@@ -1,7 +1,7 @@
 //CTRCA ///
 
-
 editAppSpecific('Expiration Date', dateAdd(null, 180));
+
 if (proximity('AGIS_CHARCO', 'Sea Turtle Lighting Zones', 1)) {
     addStdCondition('CC PERMIT', 'Sea Turtle Monitoring');
     //replaced branch(SeaTurtleMonitor)
