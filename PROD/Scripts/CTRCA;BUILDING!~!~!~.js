5a436ff94b43603321453fa6508c466ccc2cd22b
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-07-26 06:07:39 -0400

    commit by emse tools 2019-07-26 06:07:39

--- PROD/Scripts/CTRCA;BUILDING!~!~!~.js
+++ PROD/Scripts/CTRCA;BUILDING!~!~!~.js
@@ -24,74 +24,74 @@ if (appMatch('Building/Construction/*/*')) {
 }
 
 
-
 var cap = aa.cap.getCap(capId).getOutput();
 var CapTypeResult = cap.getCapType();
 var frACA = cap.isCreatedByACA();
 
+if (!appMatch('Building/Land Development/*/*')) {
 
-var profArr = new Array();
-profArr = aa.licenseScript.getLicenseProf(capId);
-profArr = profArr.getOutput();
+    var profArr = new Array();
+    profArr = aa.licenseScript.getLicenseProf(capId);
+    profArr = profArr.getOutput();
 
-aa.print("arr len=" + profArr.length);
-var pLicNo = "";
-if ((profArr != null && profArr.length > 0)) {
-    for (x in profArr) {
-        if (profArr[x].getPrintFlag() == 'Y') {
-            pLicNo = profArr[x].getLicenseNbr();
-        } else {
-            aa.print(profArr[x].getLicenseNbr() + " is not set as a primary LP.")
+    aa.print("arr len=" + profArr.length);
+    var pLicNo = "";
+    if ((profArr != null && profArr.length > 0)) {
+        for (x in profArr) {
+            if (profArr[x].getPrintFlag() == 'Y') {
+                pLicNo = profArr[x].getLicenseNbr();
+            } else {
+                aa.print(profArr[x].getLicenseNbr() + " is not set as a primary LP.")
+            }
         }
     }
+    aa.print("pLicNo:" + pLicNo);
+
+
+    if (appMatch('Building/Trade Permits/*/Mechanical') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Water Heater') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Electrical') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Fire') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/Residential/Solar Water Heater') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Accessories/*/Solar Photovoltaic') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Plumbing') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Roofing') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Gas') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/*/Pool Heat Pump') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
+
+    if (appMatch('Building/Trade Permits/Residential/Pool Solar System') && (pLicNo.substr(0, 3) == 'CGC')) {
+        addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
+    }
 }
-aa.print("pLicNo:" + pLicNo);
-
-
-if (appMatch('Building/Trade Permits/*/Mechanical') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Water Heater') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Electrical') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Fire') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/Residential/Solar Water Heater') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Accessories/*/Solar Photovoltaic') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Plumbing') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Roofing') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Gas') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/*/Pool Heat Pump') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
-if (appMatch('Building/Trade Permits/Residential/Pool Solar System') && (pLicNo.substr(0, 3) == 'CGC')) {
-    addStdCondition('CC PERMIT', 'Subcontractor Worksheet');
-}
-
 // DISABLED: ConvertToRealCapAfter:30
 // if (appMatch('Building/*/*/*')) {
 // 	var capResult = aa.cap.getCap(capId).getOutput().getCapModel();
