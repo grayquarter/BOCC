c5fe0ecbb23f4a273597de80f267af723a601c0c
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2019-07-29 11:06:53 -0400

    commit by emse tools 2019-07-29 11:06:53

--- PROD/Scripts/CTRCA;BUILDING!~!~!~.js
+++ PROD/Scripts/CTRCA;BUILDING!~!~!~.js
@@ -136,3 +136,61 @@ if (proximity('AGIS_CHARCO', 'DRI', 1) && !appMatch('Building/POS/*/*')) {
 // 	comment('Open permit at same address.  Please contact Community Development.');
 // 	cancel=true;
 // 	}
+
+
+var capPeopleArr = null;
+var peopCon = aa.people.getCapContactByCapID(capId);
+if (peopCon.getSuccess()) {
+    capPeopleArr = peopCon.getOutput();
+}
+if (capPeopleArr.length == 0) {
+    aa.print("There are no contacts associated with this record.");
+}
+
+var primFlag = 0;
+var pCon = "";
+
+
+if (capPeopleArr != null || capPeopleArr.length > 0) {
+    for (i in capPeopleArr) {
+        var capContactScriptModel = capPeopleArr[i];
+        var capContactModel = capContactScriptModel.getCapContactModel();
+        var peopleModel = capContactScriptModel.getPeople();
+
+        var thisPeop = capPeopleArr[i]; 
+        var allPeopData = thisPeop.getPeople();  
+        var firstName1 = peopleModel.getFirstName();  
+        var pEml = peopleModel.getEmail();
+        var bizName = peopleModel.getBusinessName();
+        var cType = peopleModel.getContactType();
+        var isPrim = peopleModel.getFlag();
+        var conNo = peopleModel.getContactSeqNumber();
+        if (isPrim == 'Y') {
+            primFlag++;
+            var fPcon = conNo;
+        } else {
+            if (i == 0) {
+                pCon = conNo;
+            }
+        }
+    }
+
+    if (primFlag == 0) {
+        var capContactResult = aa.people.getCapContactByPK(capId, pCon);
+        if (capContactResult.getSuccess()) {
+            var contact = capContactResult.getOutput();
+            var peopleObj = contact.getCapContactModel().getPeople();
+            peopleObj.setFlag("Y");
+            contact.getCapContactModel().setPeople(peopleObj);
+            var editResult = aa.people.editCapContact(contact.getCapContactModel());
+            if (editResult.getSuccess()) {
+                aa.print(pCon +  " contact successfully set to Primary.");
+            }
+        } else {
+            aa.print("No people contact loaded...");
+        }
+    } else {
+        aa.print(primFlag + " Primary contact found: " + fPcon);
+    }
+}
+
