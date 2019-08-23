7e668bc414db90a4918feb7847c191f489ad1a2a
klbocc <42004819+klBOCC@users.noreply.github.com>
2019-08-23 15:06:02 -0400

    Update ISA;BUILDING!CONSTRUCTION!~!~.js

--- PROD/Scripts/ISA;BUILDING!CONSTRUCTION!~!~.js
+++ PROD/Scripts/ISA;BUILDING!CONSTRUCTION!~!~.js
@@ -1,1 +1,45 @@
+if (inspType == 'Electric Final') {
+    var ElecFin = checkInspectionResult('Electric Final', 'Scheduled');
+    if (ElecFin == true) {
 
+        if (appMatch('Building/Construction/Commercial/Multi-Family')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/DCA Home')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Duplex')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Duplex-Babcock')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Mobile Home')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Modular')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Single Family')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Single Family-Babcock')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Townhouse')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+
+        if (appMatch('Building/Construction/Residential/Townhouse-Babcock')) {
+            editAppSpecific('Service Start Date', dateAdd(null, 0));
+        }
+    }
+}
