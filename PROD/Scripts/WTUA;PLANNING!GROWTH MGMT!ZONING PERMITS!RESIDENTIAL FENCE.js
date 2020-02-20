26f12d6406e6638a4dc7bf22474f42eefc55003f
SYSTEM <SYSTEM@PSC-ACLA-WEB-2.charco.gov>
2020-02-20 14:44:59 -0500

    commit by emse tools 2020-02-20 14:44:59

--- PROD/Scripts/WTUA;PLANNING!GROWTH MGMT!ZONING PERMITS!RESIDENTIAL FENCE.js
+++ PROD/Scripts/WTUA;PLANNING!GROWTH MGMT!ZONING PERMITS!RESIDENTIAL FENCE.js
@@ -1,5 +1,1 @@
-//WTUA:PLANNING/GROWTH MGMT/ZONING PERMITS/RESIDENTIAL FENCE
-
-if (wfTask == 'Permit Issuance' && wfStatus == 'Permit Renewed') {
-	editAppSpecific('Expiration Date', dateAdd(, 180));
-}
+// Enter your script here...
